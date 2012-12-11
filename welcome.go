cfdde033d581a82cd71df8818b8fc1ec984133f1
Till Salzer <till.salzer@gmail.com>
2012-12-11 12:07:32 +0100

    added some flags.
    Oh, we all love flags. Love them. Need them. Can't live without them.
    Now we have them. Feeling much better now.

--- welcome.go
+++ welcome.go
@@ -7,11 +7,24 @@ package main
 import (
     "os"
     "fmt"
+    "log"
+    "flag"
 )
 
+var flagLog bool
+
 func usage() {
-    fmt.Fprintf(os.Stderr, `Welcome.
+    if flagLog {
+        log.Printf(`Yes yes, you are welcome. Happy now?`)
+    } else {
+        fmt.Fprintf(os.Stderr, `Welcome.
 `)
+    }
+}
+
+func init() {
+    flag.BoolVar(&flagLog, "log", false, "log something else")
+    flag.Parse()
 }
 
 func main() {
