017dfe5c338a18de9880bee6947e2083dfcc71df
Till Salzer <till.salzer@gmail.com>
2013-01-25 18:08:30 +0100

    WiP: Work on CLI tests
    Test should be more simple.

--- src/pwdgen/cli/cli_test.go
+++ src/pwdgen/cli/cli_test.go
@@ -4,6 +4,27 @@ import (
     "testing"
 )
 
+func concatStringSlices(old1, old2 []string) []string {
+    newslice := make([]string, len(old1) + len(old2))
+    copy(newslice, old1)
+    copy(newslice[len(old1):], old2)
+    return newslice
+}
+// 
+// func cliLoader(t *testing.T, args []string, cb func(c Cli, a []string)) {
+//     fullargs = concatStringClices([]string{"x"}, args)
+//     if cl, err := ParseNewCliFromString(fullargs); err != nil {
+//         t.Errorf("failed to parse %s, results in error: %s", args, err)
+//     } else {
+//         fn(cl, fullargs)
+//     }
+// }
+// 
+// func cliLoaderiSingle(t *testing.T, arg string, cb func(c Cli, a []string)) {
+//     args = []string{arg}
+//     cliLoader(args)
+// }
+
 func TestNewCli(t *testing.T) {
     cli := NewCli()
 
@@ -17,8 +38,12 @@ func TestParseFriendly(t *testing.T) {
     var err error
     var args []string
 
-    loader := func(args []string, fn func()) {
-        if cl, err = ParseNewCliFromString(args); err != nil {
+
+
+    loader := func(tstargs []string, fn func()) {
+        args = tstargs
+        fullargs := concatStringSlices([]string{"x"}, args)
+        if cl, err = ParseNewCliFromString(fullargs); err != nil {
             t.Errorf("failed to parse %s, results in error: %s", args, err)
         } else {
             fn()
@@ -26,29 +51,32 @@ func TestParseFriendly(t *testing.T) {
     }
 
     // password length, flags style
-    loader([]string{"x", "-l", "10"}, func(){ if cl.PwLength != 10 {
+    loader([]string{"-l", "10"}, func(){ if cl.PwLength != 10 {
             t.Errorf("expected %s to set password length to 10, got %d", args, cl.PwLength)
         } })
 
 
-    loader([]string{"x", "-0"}, func(){ if cl.UseNumbers != false {
+    // cliLoaderSingle(t, "-0", func(c Cli) { if c.UseNumber != false {
+    //     t.Errorf("expected %s to set UseNumber to false, got %s" a, c.UseNumbers)
+    // }
+    loader([]string{"-0"}, func(){ if cl.UseNumbers != false {
             t.Errorf("expected %s to set UseNumbers to false, got %b", args, cl.UseNumbers)
         } })
-    loader([]string{"x", "--no-numerals"}, func(){ if cl.UseNumbers != false {
+    loader([]string{"--no-numerals"}, func(){ if cl.UseNumbers != false {
             t.Errorf("expected %s to set UseNumbers to false, got %b", args, cl.UseNumbers)
         } })
-    loader([]string{"x", "-n"}, func(){ if cl.UseNumbers != true {
+    loader([]string{"-n"}, func(){ if cl.UseNumbers != true {
             t.Errorf("expected %s to set UseNumbers to true, got %b", args, cl.UseNumbers)
         } })
-    loader([]string{"x", "--numerals"}, func(){ if cl.UseNumbers != true {
+    loader([]string{"--numerals"}, func(){ if cl.UseNumbers != true {
             t.Errorf("expected %s to set UseNumbers to true, got %b", args, cl.UseNumbers)
         } })
 
 
-    loader([]string{"x", "-h"}, func(){ if cl.ShowHelp != true {
+    loader([]string{"-h"}, func(){ if cl.ShowHelp != true {
             t.Errorf("expected %s to set ShowHelp to true, got %b", args, cl.ShowHelp)
         } })
-    loader([]string{"x", "--help"}, func(){ if cl.ShowHelp != true {
+    loader([]string{"--help"}, func(){ if cl.ShowHelp != true {
             t.Errorf("expected %s to set ShowHelp to true, got %b", args, cl.ShowHelp)
         } })
 }
