1989426accc8322989311c13d4a10ca942a43262
Till Salzer <till.salzer@gmail.com>
2013-01-25 15:30:35 +0100

    Add simple command line parser
    The simple command line parser, which reads most of pwgens CLI, does
    work *except* for the assigning arguments. So you can use "-N 2" to get
    2 passwords, but not "--num-passwords=2" (but "--num-passwords 2").
    Still work to be done here, but maybe this should be postponed for a
    more generic implementation.
    
    The positional arguments are still not evaluated.
    
    Finally, the parser tests are clumsy and incomplete, which is a bad
    combination.

--- src/pwdgen/cli/cli.go
+++ src/pwdgen/cli/cli.go
@@ -11,8 +11,29 @@ import (
 var commandLine = os.Args[0]
 
 type Cli struct {
-    PwLength int
-    ShowCharsets bool
+    // basic options
+    PwLength int        // number of characters in the final password
+    PwNum int           // number of passwords to print (0 prints as much as possible)
+
+    // select generator
+    Secure bool         // true: use completely random symbols, false: memorable
+
+    // select symbol sets and filters
+    UseNumbers bool     // true: must use numbers, false: don't care
+    UseNoVowels bool    // true: don't use vowels, false: don't care
+    UseSpecials bool    // true: use specials, false: don't care
+    UseAmbigous bool    // true: no letters with might be confused, false: don't care
+    UseCapitals bool    // true: must use a capital, false: don't care
+
+    // output options
+    PrintOnePerLine bool // print one password per line
+    PrintColumns bool   // print passwords in columns
+
+    // options controlling what we do
+    ShowHelp bool       // show help and exit
+    ShowCharsets bool   // show charsets and exit
+
+    // the arguments, unparsed
     Args []string
 }
 
@@ -25,6 +46,22 @@ const DefaultPwLength = 8
 func createCli() Cli {
     var retval Cli
     retval.PwLength = DefaultPwLength
+    retval.PwNum = 0
+
+    retval.Secure = true
+
+    retval.UseNumbers = false
+    retval.UseNoVowels = false
+    retval.UseSpecials = false
+    retval.UseAmbigous = false
+    retval.UseCapitals = false
+
+    retval.PrintOnePerLine = false
+    retval.PrintColumns = true
+
+    retval.ShowHelp = false
+    retval.ShowCharsets = false
+
     return retval
 }
 
@@ -35,10 +72,22 @@ func NewCli() Cli {
     return retval
 }
 
+func ParseNewCli() (Cli, error) {
+    retval := NewCli()
+    err := retval.Parse()
+    return retval, err
+}
+
 func NewCliFromString(args []string) Cli {
     retval := createCli()
     retval.Args = args
     return retval
+}
+
+func ParseNewCliFromString(args []string) (Cli, error) {
+    retval := NewCliFromString(args)
+    err := retval.Parse()
+    return retval, err
 }
 
 // parse the command line stored in this Cli object.
@@ -50,8 +99,8 @@ func (c *Cli) Parse() error {
 
     for idx = 1; idx < len(c.Args); idx++ {
         val = c.Args[idx]
-        switch val {
-        case "-l":
+        switch {
+        case val == "-l":
             idx++
             val2 := c.Args[idx]
             if ival64,err := strconv.ParseInt(val2, 10, 32); err == nil {
@@ -59,6 +108,40 @@ func (c *Cli) Parse() error {
             } else {
                 return fmt.Errorf("not a password length: \"%s\" (%s)", val2, err)
             }
+        case val == "-0" || val == "--no-numerals":
+            c.UseNumbers = false
+        case val == "-n" || val == "--numerals":
+            c.UseNumbers = true
+        case val == "-1":
+            c.PrintOnePerLine = true
+        case val == "-A" || val == "--no-capitalize":
+            c.UseCapitals = false
+        case val == "--capitals":
+            c.UseCapitals = true
+        case val == "-a" || val == "--alt-phonics":
+            // ignore
+        case val == "-B" || val == "--ambigous":
+            c.UseAmbigous = true
+        case val == "-C":
+            c.PrintColumns = true
+        case val == "-N" || val == "--num-password":
+            idx++
+            val2 := c.Args[idx]
+            if ival64,err := strconv.ParseInt(val2, 10, 32); err == nil {
+                c.PwNum = int(ival64)
+            } else {
+                return fmt.Errorf("not a number of passwords: \"%s\" (%s)", val2, err)
+            }
+        case val == "-H" || val == "--sha1":
+            // ignore
+        case val == "-h" || val == "--help":
+            c.ShowHelp = true
+        case val == "-s" || val == "--secure":
+            c.Secure = true
+        case val == "-v" || val == "--no-vowels":
+            c.UseNoVowels = true
+        case val == "-y" || val == "--symbols":
+            c.UseSpecials = true
         default:
             return fmt.Errorf("unknwon argument \"%s\" at %d", val, idx)
         }
--- src/pwdgen/cli/cli_test.go
+++ src/pwdgen/cli/cli_test.go
@@ -12,3 +12,44 @@ func TestNewCli(t *testing.T) {
     }
 }
 
+func TestParseFriendly(t *testing.T) {
+    var cl Cli
+    var err error
+    var args []string
+
+    loader := func(args []string, fn func()) {
+        if cl, err = ParseNewCliFromString(args); err != nil {
+            t.Errorf("failed to parse %s, results in error: %s", args, err)
+        } else {
+            fn()
+        }
+    }
+
+    // password length, flags style
+    loader([]string{"x", "-l", "10"}, func(){ if cl.PwLength != 10 {
+            t.Errorf("expected %s to set password length to 10, got %d", args, cl.PwLength)
+        } })
+
+
+    loader([]string{"x", "-0"}, func(){ if cl.UseNumbers != false {
+            t.Errorf("expected %s to set UseNumbers to false, got %b", args, cl.UseNumbers)
+        } })
+    loader([]string{"x", "--no-numerals"}, func(){ if cl.UseNumbers != false {
+            t.Errorf("expected %s to set UseNumbers to false, got %b", args, cl.UseNumbers)
+        } })
+    loader([]string{"x", "-n"}, func(){ if cl.UseNumbers != true {
+            t.Errorf("expected %s to set UseNumbers to true, got %b", args, cl.UseNumbers)
+        } })
+    loader([]string{"x", "--numerals"}, func(){ if cl.UseNumbers != true {
+            t.Errorf("expected %s to set UseNumbers to true, got %b", args, cl.UseNumbers)
+        } })
+
+
+    loader([]string{"x", "-h"}, func(){ if cl.ShowHelp != true {
+            t.Errorf("expected %s to set ShowHelp to true, got %b", args, cl.ShowHelp)
+        } })
+    loader([]string{"x", "--help"}, func(){ if cl.ShowHelp != true {
+            t.Errorf("expected %s to set ShowHelp to true, got %b", args, cl.ShowHelp)
+        } })
+}
+
